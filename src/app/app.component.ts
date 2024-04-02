f00cce22ebc8e832732fafd8f0fceafe163b47de
jeisonj2019 <jeisonj2019@gmail.com>
2024-04-02 08:21:01 +0500

    Форматирование

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,17 @@
-import { AfterViewChecked, AfterViewInit, ChangeDetectorRef, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import {
+  AfterViewChecked,
+  AfterViewInit,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./components/header/header.component";
-import { AddFormComponent } from "./components/add-form/add-form.component";
+import { HeaderComponent } from './components/header/header.component';
+import { AddFormComponent } from './components/add-form/add-form.component';
 import { RecordItem } from './models/interfaces';
-import { ListComponent } from "./components/list/list.component";
+import { ListComponent } from './components/list/list.component';
 import { RecordService } from './services/record.service';
 
 @Component({
@@ -11,62 +19,73 @@ import { RecordService } from './services/record.service';
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterOutlet, HeaderComponent, AddFormComponent, ListComponent]
+  imports: [RouterOutlet, HeaderComponent, AddFormComponent, ListComponent],
 })
-
 export class AppComponent implements OnInit, AfterViewInit {
+  constructor(private recordService: RecordService) {}
+
   @ViewChild('searchElem') searchElem!: ElementRef<HTMLInputElement>;
-  search() {
-    var nativeElement = this.searchElem.nativeElement;
-    var searchValue = nativeElement.value;
-
-    if (this.searchKeys.some(key => searchValue.endsWith(key))) {
-      searchValue = '';
-      nativeElement.value = searchValue;
-      this.search();
-      return;
-    }
-
-    if (searchValue) {
-      this.searchNow = true;
-      this.found = this.records.filter(rec=>`${rec.url}+${rec.command}`.toLowerCase().includes(searchValue.toLowerCase()));
-    }
-    else {
-      this.searchNow = false;
-    }
-  }
-  searchKeys: string[] = ['`','ё'];
+  
+  searchKeys: string[] = ['Escape'];
   searchNow: any;
   found: RecordItem[];
 
-  constructor(
-    private recordService: RecordService
-  ) { }
   records: RecordItem[] = [];
-  recordServiceSubscription = this.recordService.signal$.subscribe((recs: RecordItem[]) => {
-    this.records = recs;
-    this.found = this.records;
-    this.search();
-  });
+
+  recordServiceSubscription = this.recordService.signal$.subscribe(
+    (recs: RecordItem[]) => {
+      this.records = recs;
+      this.found = this.records;
+      this.search();
+    }
+  );
+
+
+
+  // ****************************************************************
 
   ngOnInit(): void {
     this.records = this.recordService.list();
+  }
 
-  }
-  handleSearchKeydown(event: KeyboardEvent): void {
-    if (this.searchKeys.includes(event.key)) {
-      this.searchElem.nativeElement.focus();
-      this.search();
-    }
-  }
   ngAfterViewInit(): void {
     document.addEventListener('keydown', this.handleSearchKeydown.bind(this));
   }
   ngOnDestroy(): void {
-    document.removeEventListener('keydown', this.handleSearchKeydown.bind(this));
+    document.removeEventListener(
+      'keydown',
+      this.handleSearchKeydown.bind(this)
+    );
   }
+
+  // ***************************************************************
+
+  handleSearchKeydown(event: KeyboardEvent) {
+    if (this.searchKeys.includes(event.key)) {
+      this.searchElem.nativeElement.focus();
+      this.searchElem.nativeElement.value = '';
+      this.search();
+
+    }
+  }
+
 
   addRecord($event: RecordItem) {
     this.records = this.recordService.add($event);
+  }
+  search() {
+    var nativeElement = this.searchElem.nativeElement;
+    var searchValue = nativeElement.value;
+
+    if (searchValue) {
+      this.searchNow = true;
+      this.found = this.records.filter((rec) =>
+        `${rec.url}+${rec.command}`
+          .toLowerCase()
+          .includes(searchValue.toLowerCase())
+      );
+    } else {
+      this.searchNow = false;
+    }
   }
 }
